f953164d0de24271284ad291e31c967a802b50f3
afifi brahim <afifi71brahim@gmail.com>
2024-01-17 22:57:59 +0100

    add desktop navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,25 @@ const Navbar = () => {
   return (
     <div className='' >
        {/* desktop navbar */}
-        <nav className='w-[93%] min-h-[15vh]    m-auto flex itmes-center justify-center'>
+        <nav className=' w-[97%] 2xl:w-[93%] min-h-[15vh] hidden   m-auto lg:flex itmes-center justify-center'>
              <div className='flex w-full items-center justify-between'>
                  <div>
                     <img className='w-40' src={Logo} alt="" />
                  </div>
-                 <ul className='flex text-pimaryBlue   2xl:text-lg items-center gap-6 2xl:gap-16'>
-                    <a className='hover:text-blue-700 duration-500' href='#'>Accueil</a>
-                    <a className='hover:text-blue-700 duration-500' href='#'>Notre Ecole</a>
-                    <a className='hover:text-blue-700 duration-500' href='#'>Academique</a>
-                    <a className='hover:text-blue-700 duration-500' href='#'>Admessions</a>
-                    <a className='hover:text-blue-700 duration-500' href='#'>Vie Scolaire</a>
-                    <a className='hover:text-blue-700 duration-500' href='#'>Actualitees</a>
-                    <a className='hover:text-blue-700 duration-500' href='#'>Conctactez nous</a>
-                    <a className='text-white duration-500 py-2 bg-lightYellow px-2 rounded-md' href='#'>Inscriptions</a>
+                 <ul className='flex text-pimaryBlue text-xs md:text-sm lg:text-md  2xl:text-lg items-center gap-4 xl:gap-6 2xl:gap-16'>
+                    <a className='hover:text-yellow-500 bg-lightYellow py-1 px-1 rounded-md duration-500' href='#'>Accueil</a>
+                    <a className='hover:text-yellow-500 duration-500' href='#'>Notre Ecole</a>
+                    <a className='hover:text-yellow-500 duration-500' href='#'>Academique</a>
+                    <a className='hover:text-yellow-500 duration-500' href='#'>Admessions</a>
+                    <a className='hover:text-yellow-500 duration-500' href='#'>Vie Scolaire</a>
+                    <a className='hover:text-yellow-500 duration-500' href='#'>Actualitees</a>
+                    <a className='hover:text-yellow-500 duration-500' href='#'>Conctactez nous</a>
+                    <a className='text-white duration-500 py-4 font-semibold bg-lightYellow px-2 rounded-md' href='#'>Inscriptions</a>
                  </ul>
              </div>
         </nav>
-
+       {/* mobile navbar */}
+       
 
     </div>
   )
