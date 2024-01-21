5310634dec4b5e7dba5d557d1e703917a6db616f
afifi brahim <afifi71brahim@gmail.com>
2024-01-21 13:20:52 +0100

    fix some chart style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
         <div className='flex items-center justify-center'>
             <img className='w-[60%] lg:w-auto' src={headerimg} alt="" />
         </div>
-    <h1 className='uppercase absolute text-white text-xl md:text-3xl lg:text-5xl 2xl:text-7xl right-[5%] md:right-[15%] top-[30%] '>jusqu'au <span className='text-lightYellow'>lycee</span></h1>
-    <h1 className='uppercase absolute text-white text-xl md:text-3xl lg:text-5xl 2xl:text-7xl left-[5%] md:left-[15%] bottom-[30%]'>de la <span className='text-lightYellow'>maternelle</span></h1>
+    <h1 className='uppercase absolute text-white text-xl md:text-3xl lg:text-5xl 2xl:text-7xl right-[5%] md:right-[15%] top-[30%] '>jusqu'au <span className='text-lightYellow font-bold'>lycee</span></h1>
+    <h1 className='uppercase absolute text-white text-xl md:text-3xl lg:text-5xl 2xl:text-7xl left-[5%] md:left-[15%] bottom-[30%]'>de la <span className='text-lightYellow font-bold'>maternelle</span></h1>
     </div>
   )
 }
